0cb4c7764233321bb111adbc75b409eabc3dd090
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-06-25 21:58:31 -0300

    update: removing unnecessary imports and treating exceptions

--- scraper.py
+++ scraper.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-from playwright.sync_api import sync_playwright
 
 URL_BASE = "https://ffp-pi.com.br/competicoes"
 
@@ -49,16 +48,21 @@ def get_tabelas(idcampeonato:str): #função de pegar partidas dentro das compet
     return fases
 
 def baixar_bordero(id_partida:str):
-
     URL_BORDERO = f"https://conteudo.cbf.com.br/federacoes/10/borderos/2025/{id_partida}b.pdf"
-    response = requests.get(URL_BORDERO)
-
-    if response.status_code == 200:
-        nome_arquivo = f"bordero_{id_partida}.pdf"
-        with open(nome_arquivo, "wb") as f:
-            f.write(response.content)
-        print(f"✅ PDF salvo como: {nome_arquivo}")
-        return URL_BORDERO
-    else:
-        print(f"❌ Borderô não encontrado para o jogo {id_partida}")
-        return None
+    try:
+        response = requests.get(URL_BORDERO, timeout=10)
+        if response.status_code == 200:
+            nome_arquivo = f"bordero_{id_partida}.pdf"
+            with open(nome_arquivo, "wb") as f:
+                f.write(response.content)
+                print(f"✅ PDF salvo como: {nome_arquivo}")
+            return URL_BORDERO
+        else:
+            print(f"❌ Borderô não encontrado para o jogo {id_partida} (Status: {response.status_code})")
+            return None
+    except SSLError as e:
+        print(f"❌ Erro SSL ao tentar baixar borderô: {e}")
+        return {"erro": "Erro SSL ao tentar acessar o PDF", "detalhes": str(e)}
+    except Exception as e:
+        print(f"❌ Erro inesperado ao tentar baixar o borderô: {e}")
+        return {"erro": "Erro inesperado", "detalhes": str(e)}
