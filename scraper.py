b131ae941bd06b08403981e5fa70f4c24d8d8f40
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-06-26 08:36:57 -0300

    update: inserting page headers

--- scraper.py
+++ scraper.py
@@ -25,8 +25,10 @@ def get_campeonato(): #função de buscar competições
 def get_tabelas(idcampeonato:str): #função de pegar partidas dentro das competições
 
     URL_TABELA = f"https://ffp-pi.com.br/competicoes/tabela/{idcampeonato}"
-
-    r = requests.get(URL_TABELA)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+    }
+    r = requests.get(URL_TABELA,headers=headers)
     soup = BeautifulSoup(r.text, 'html.parser')
     fases = []
     #print(soup.prettify())
@@ -50,7 +52,10 @@ def get_tabelas(idcampeonato:str): #função de pegar partidas dentro das compet
 def baixar_bordero(id_partida:str):
     URL_BORDERO = f"https://conteudo.cbf.com.br/federacoes/10/borderos/2025/{id_partida}b.pdf"
     try:
-        response = requests.get(URL_BORDERO, timeout=10)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+        }
+        response = requests.get(URL_BORDERO,headers=headers,timeout=10)
         if response.status_code == 200:
             nome_arquivo = f"bordero_{id_partida}.pdf"
             with open(nome_arquivo, "wb") as f:
